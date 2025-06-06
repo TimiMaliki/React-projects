24999d3796dfb66dfad1f2d85ad7f53fb5161730
TimiMaliki <malikitimi@gmail.com>
2025-06-06 02:29:44 +0100

    studio section

--- frontend-portfolio/dianceblog/src/component/homeComponents/studio.jsx
+++ frontend-portfolio/dianceblog/src/component/homeComponents/studio.jsx
@@ -3,27 +3,31 @@ const Studio = () => {
     <section className="w-full h-full  grid lg:grid-cols-3 lg:p-12">
       <div className="card-one w-full h-full  text-center">
         <h1 className="text-5xl lg:text-7xl font-extrabold mt-2 p-8">
-          Empowering Leading Industries with Maliki-Studio
+          Make Your Dreams Come True with Diance-Blog
         </h1>
       </div>
 
       <div className="card-one w-full h-full  text-center">
-        <h1 className="text-2xl font-extrabold p-2">Meet Maliki-Studio</h1>
+        <h1 className="text-2xl font-extrabold p-2">Meet Diance-Blog</h1>
       </div>
 
       <div className="card-one w-full h-full  text-center">
         <p className="text-xl   p-10">
-          "At Maliki-Studio, we specialize in unleashing the full potential of
-          your brand through advanced digital solutions. <br /> <br />
-          With a focus on UX/UI Design, Branding, and E-commerce, our expertise
-          is tailored to create impactful digital experiences across Healthcare,
-          SaaS, FinTech, Real Estate, E-commerce, and AI sectors. <br /> <br />
-          Our approach is budget-conscious, meticulously planning and
-          rationalizing expenses to deliver a cost-effective digital strategy.
-          Our aim is to provide you with a profitable product strategy and an
-          efficient execution plan, ensuring maximum returns on your investment.
-          Partner with us to position your brand as a leader in its industry,
-          thriving in the ever-evolving online landscape.
+          At Diance-Blog, we specialize exclusively in bringing blog ideas to
+          life — whether you're starting a personal vlog or launching a
+          full-scale blog platform. <br />
+          <br />
+          From solo content creators to growing blog enterprises, our mission is
+          to turn your vision into a seamless, engaging digital experience. We
+          focus solely on blog creation, ensuring that every design decision and
+          technical feature supports your goals, your audience, and your voice.
+          <br />
+          <br />
+          Whether it's a minimalist personal journal or a content-rich blogging
+          network, we offer tailored solutions that are budget-conscious and
+          strategically planned to scale with your needs. Partner with us to
+          build a blog platform that not only reflects your identity but also
+          stands out in today’s fast-paced digital world.
         </p>
       </div>
     </section>
