c11c2ae5fd2d9daa18e16b54e89ac39fe7ed0ce5
TimiMaliki <malikitimi@gmail.com>
2025-06-10 20:13:46 +0100

    added : blog columns section

--- frontend-portfolio/dianceblog/src/NewsPages/LatestStoryBox.jsx
+++ frontend-portfolio/dianceblog/src/NewsPages/LatestStoryBox.jsx
@@ -1,17 +1,17 @@
 const Latest = () => {
     return ( 
         <div className="w-full">
-               <div className="box bg-blue-300 w-96 h-80 shadow-lg rounded-lg mt-12">
-                  <div className="head text-xl text-left p-5">
+               <div className="box bg-blue-300 w-full h-80 shadow-lg rounded-lg mt-12">
+                  <div className="head text-xl font-bold text-left p-5">
                     <p>latest</p>
                   </div>
-                  <div className="body text-3xl text-left p-5">
+                  <div className="body text-3xl font-extrabold text-left p-5">
                      <h2>
                         Inspiration from leaders <br />
                         and Communities around the world
                      </h2>
                   </div>
-                  <div className="viewMore text-left p-5">
+                  <div className="viewMore  font-semibold text-left p-5">
                     <p>View All Stories</p>
                   </div>
                </div>
