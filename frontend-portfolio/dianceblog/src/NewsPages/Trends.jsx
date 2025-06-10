37cf4e9ebd8ca9ee1afc0c29db845c873039be45
TimiMaliki <malikitimi@gmail.com>
2025-06-10 01:00:38 +0100

    installed react-router and router dom

--- frontend-portfolio/dianceblog/src/NewsPages/Trends.jsx
+++ frontend-portfolio/dianceblog/src/NewsPages/Trends.jsx
@@ -3,8 +3,8 @@ import  heroImg  from "../images/blog-hero.jpg"
 const TrendingNews = () => {
     return ( 
         <div className="mt-4 w-full h-screen ">
-          
-           <img src={heroImg} alt=""  className="w-full h-full object-contain"/>
+        <h1 className="flex justify-center items-center text-center">Hello</h1>  
+        
         </div>
      );
 }
