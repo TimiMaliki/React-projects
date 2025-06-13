0e009015aa68b01b1220a5367277881f050f03ff
TimiMaliki <malikitimi@gmail.com>
2025-06-13 12:23:52 +0100

    added : some blogs

---
+++ frontend-portfolio/dianceblog/src/NewsPages/ApplePage/AppleBlog/AppleBlog.jsx
@@ -0,0 +1,52 @@
+import useFetchAPI from "../AppleUsefetch/useFetchAPI";
+
+const AppleBlog = () => {
+    const {data} = useFetchAPI("https://api.marketaux.com/v1/news/all?limit=4&api_token=ezKXBl1EusNWsCr41LKde59CMVjkXHcs6TJX9mNo")
+  return (
+    <div>
+       <div className="grid place-content-center">
+      {data&&data.data.map((blog, index) => (
+          <div className="" key={index}>
+            <div className="card-one">
+              <div className="col-span-6 md:col-span-3 lg:col-span-2 mb-3 mt-12">
+                <article className="rounded-lg">
+                  <div className="relative">
+                    <img
+                      src={blog.image_url}
+                      alt="Blog"
+                      className="h-auto w-full rounded-lg shadow-lg dark:shadow-none"
+                    />
+                    <div className="absolute bottom-2 left-2 text-lg leading-6  px-3 py-2 lg:px-6 lg:py-3 font-black bg-slate-500 opacity-80 rounded-lg">
+                      {blog.published_at}
+                    </div>
+                  </div>
+                  <div className="p-3">
+                    <p className="font-light text-sm leading-6 mb-2">
+                      By {blog.author}
+                   
+                      <a href="#" className="text-blue-600">
+                        {blog.title}
+                      </a>
+                    </p>
+                    <h4 className="font-medium text-2xl mb-6">
+                      {blog.description}
+                    </h4>
+                    <p className="opacity-60 mt-3 mb-6">{blog.snippet}</p>
+                    <a
+                      href={blog.url}
+                      className="bg-transparent hover:bg-blue-600 border border-blue-600 hover:text-white py-2 px-5 rounded transition"
+                    >
+                      Read More
+                    </a>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+    </div>
+  );
+};
+
+export default AppleBlog;
--- frontend-portfolio/dianceblog/src/NewsPages/BlogCols.jsx
+++ frontend-portfolio/dianceblog/src/NewsPages/BlogCols.jsx
@@ -3,6 +3,7 @@ import DataTwo from "./BusinessPage/BussinessBlog/bussinessData";
 import CarsData from "./CarsPage/CarsBlog/CarsData";
 import Latest from "./LatestStoryBox";
 import Data from "./TechPage/TechBlog/techData";
+import AppleBlog from "./ApplePage/AppleBlog/AppleBlog";
 
 
 
@@ -33,7 +34,7 @@ const BlogCols = () => {
       </div>
 
       <Border />
-      <div className="grid grid-cols-2 gap-3 mt-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 mt-4">
         <div className="first-grid">
           <Latest
             head={"latest"}
@@ -53,115 +54,17 @@ const BlogCols = () => {
         <div className="first-grid hidden lg:block">
           <Latest
             head={"latest"}
-            body={"Inspiration from leaders and Communities around the world "}
+            body={"Covering Finance and Markets Worldwide — Stay Informed, Wherever You Are"}
             viewMore={"View All Stories"}
-            bgColor="bg-yellow-900"
+            bgColor="bg-red-500"
           />
         </div>
 
         <div className="second-grid flex justify-center gap-3">
           <div className="divider w-1 h-full bg-slate-800"></div>
-          <div className="grid place-content-center">
-            <div className="latest-col">
-              <Latest
-                head={"latest"}
-                body={
-                  "Inspiration from leaders and Communities around the world "
-                }
-                viewMore={"View All Stories"}
-                bgColor="bg-pink-900"
-              />
-              <p className="mt-2">
-                We’re building a world-class museum and public gathering space
-                on the South Side of Chicago, where people from across the
-                street or around the globe can come to get inspired, find common
-                ground, and take action.
-              </p>
-            </div>
-            <div className="card-one">
-              <div className="col-span-6 md:col-span-3 lg:col-span-2 mb-3 mt-12">
-                <article className="rounded-lg">
-                  <div className="relative">
-                    <img
-                      src="https://cdn.easyfrontend.com/pictures/blog/blog_3.jpg"
-                      alt="Blog"
-                      className="h-auto w-full rounded-lg shadow-lg dark:shadow-none"
-                    />
-                    <div className="absolute bottom-2 left-2 text-lg leading-6 px-6 py-3 font-black bg-white opacity-80 rounded-lg">
-                      26
-                      <br />
-                      Oct
-                      <br />
-                      2016
-                    </div>
-                  </div>
-                  <div className="p-3">
-                    <p className="font-light text-sm leading-6 mb-2">
-                      By{" "}
-                      <a href="#" className="text-blue-600">
-                        Mahws Georgia
-                      </a>
-                    </p>
-                    <h4 className="font-medium text-2xl">
-                      Dolor sit amet, consectetur adiplscing eliyt sed
-                    </h4>
-                    <p className="opacity-60 mt-3 mb-6">
-                      Assumenda non repellendus distinctio nihil dicta sapiente,
-                      quibusdam maiores.
-                    </p>
-                    <a
-                      href="#"
-                      className="bg-transparent hover:bg-blue-600 border border-blue-600 hover:text-white py-2 px-5 rounded transition"
-                    >
-                      Read More
-                    </a>
-                  </div>
-                </article>
-              </div>
-            </div>
-            <div className="card-one">
-              <div className="col-span-6 md:col-span-3 lg:col-span-2 mb-3 mt-12">
-                <article className="rounded-lg">
-                  <div className="relative">
-                    <img
-                      src="https://cdn.easyfrontend.com/pictures/blog/blog_3.jpg"
-                      alt="Blog"
-                      className="h-auto w-full rounded-lg shadow-lg dark:shadow-none"
-                    />
-                    <div className="absolute bottom-2 left-2 text-lg leading-6 px-6 py-3 font-black bg-white opacity-80 rounded-lg">
-                      26
-                      <br />
-                      Oct
-                      <br />
-                      2016
-                    </div>
-                  </div>
-                  <div className="p-3">
-                    <p className="font-light text-sm leading-6 mb-2">
-                      By{" "}
-                      <a href="#" className="text-blue-600">
-                        Mahws Georgia
-                      </a>
-                    </p>
-                    <h4 className="font-medium text-2xl">
-                      Dolor sit amet, consectetur adiplscing eliyt sed
-                    </h4>
-                    <p className="opacity-60 mt-3 mb-6">
-                      Assumenda non repellendus distinctio nihil dicta sapiente,
-                      quibusdam maiores.
-                    </p>
-                    <a
-                      href="#"
-                      className="bg-transparent hover:bg-blue-600 border border-blue-600 hover:text-white py-2 px-5 rounded transition"
-                    >
-                      Read More
-                    </a>
-                  </div>
-                </article>
-              </div>
-            </div>
-          </div>
+        <AppleBlog/>
         </div>
+        
       </div>
     </div>
   );
