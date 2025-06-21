6bf81e94b282629f700f4a3345df68edf1a4d6e9
TimiMaliki <malikitimi@gmail.com>
2025-06-21 10:05:17 +0100

    auth layout

--- frontend-portfolio/LightRun/src/sign/SignIn.jsx
+++ frontend-portfolio/LightRun/src/sign/SignIn.jsx
@@ -16,7 +16,7 @@ const SignInPage = () => {
   };
 
   return (
-    <div className="min-h-screen  flex items-center justify-center p-4">
+    <div className="min-h-screen  flex items-center justify-center p-4 mt-10">
       <motion.div 
         initial={{ opacity: 0, scale: 0.95 }}
         animate={{ opacity: 1, scale: 1 }}
