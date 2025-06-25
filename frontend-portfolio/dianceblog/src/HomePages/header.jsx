a37b067dfcefc38a007ac6f7c3c735aea43e91bb
TimiMaliki <malikitimi@gmail.com>
2025-06-25 09:31:03 +0100

    knowledge app

--- frontend-portfolio/dianceblog/src/HomePages/header.jsx
+++
@@ -1,9 +0,0 @@
-const Headers = ({title}) => {
-    return ( 
-        <div className="header">
-     {title}
-        </div>
-     );
-}
- 
-export default Headers;
--- frontend-portfolio/dianceblog/src/NewsPages/Border.jsx
+++
@@ -1,5 +0,0 @@
-const Border = () => {
-  return <div className="w-full  border border-slate-900 mt-8"></div>;
-};
-
-export default Border;
