47c074f7a4240e52d8274a2cf05fcbb1f7ef159c
TimiMaliki <malikitimi@gmail.com>
2025-06-25 16:47:17 +0100

    exams logic

---
+++ frontend-portfolio/knowledge/src/test/questions.js
@@ -0,0 +1,20 @@
+export const questions = [
+    {
+      id: 1,
+      question: "What is the capital of France?",
+      options: ["Berlin", "Madrid", "Paris", "Rome"],
+      answer: "Paris", 
+    },
+    {
+      id: 2,
+      question: "Who wrote 'To Kill a Mockingbird'?",
+      options: ["Harper Lee", "Mark Twain", "Ernest Hemingway", "Toni Morrison"],
+      answer: "Harper Lee", 
+    },
+    {
+      id: 3,
+      question: "What is 2 + 2?",
+      options: ["3", "4", "5", "6"],
+      answer: "4", 
+    },
+  ];
