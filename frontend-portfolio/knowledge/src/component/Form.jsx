bcd93af7fc7ef2ae53c45880f1a6c4d40af66e5e
TimiMaliki <malikitimi@gmail.com>
2025-06-28 10:40:49 +0100

    two projects[responsive btn] done

--- frontend-portfolio/knowledge/src/component/Form.jsx
+++ frontend-portfolio/knowledge/src/component/Form.jsx
@@ -9,11 +9,14 @@ const HeaderForm = () => (
             "Want to know how smart you are!?"
           </p>
         </div>
-        <Link to={"/test"}>
-          <button className="absolute right-[144px] md:right-6 top-12 md:top-7 -translate-y-1/2 py-2 px-5 md:py-2  md:px-8 text-sm md:text-base text-white font-bold bg-blue-600 hover:bg-opacity-90 transition duration-300 z-50 rounded-full">
+		<div className="flex justify-center">
+		<Link to={"/test"}>
+          <button className="absolute right-[120px] md:right-6 top-12 md:top-7 -translate-y-1/2 py-2 px-5 md:py-2  md:px-8 text-sm md:text-base text-white font-bold bg-blue-600 hover:bg-opacity-90 transition duration-300 z-50 rounded-full">
             Click Here
           </button>
         </Link>
+		</div>
+        
       </div>
     </div>
   </form>
