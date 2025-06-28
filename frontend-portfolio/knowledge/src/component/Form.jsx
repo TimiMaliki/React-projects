dc89c7ec90657741127eb5710fcc235212ed77c8
TimiMaliki <malikitimi@gmail.com>
2025-06-28 10:44:39 +0100

    two projects[responsive btn] done

--- frontend-portfolio/knowledge/src/component/Form.jsx
+++ frontend-portfolio/knowledge/src/component/Form.jsx
@@ -1,22 +1,21 @@
 import { Link } from "react-router-dom";
 
 const HeaderForm = () => (
-  <form>
-    <div className="form-group">
-      <div className="input-group flex justify-center relative">
-        <div className="min-h-[80px] lg:min-h-[62px] leading-7 z-10 bg-gray-100 px-8 dark:bg-slate-800 focus:outline-none rounded-full ps-4 w-full mb-2 lg:mb-4">
-          <p className=" mt-2 lg:mt-4 text-sm lg:text-md font-bold text-center text-white">
-            "Want to know how smart you are!?"
-          </p>
-        </div>
-		<div className="flex justify-center">
-		<Link to={"/test"}>
-          <button className="absolute right-[120px] md:right-6 top-12 md:top-7 -translate-y-1/2 py-2 px-5 md:py-2  md:px-8 text-sm md:text-base text-white font-bold bg-blue-600 hover:bg-opacity-90 transition duration-300 z-50 rounded-full">
+	<form>
+    <div className="form-group w-full">
+      <div className="bg-gray-100 dark:bg-slate-800 rounded-full p-4 md:p-6 flex flex-col md:flex-row items-center justify-between w-full gap-4">
+        <p className="text-sm md:text-base font-bold text-white text-center md:text-left">
+          "Want to know how smart you are!?"
+        </p>
+
+        <Link to="/test">
+          <button
+            type="button"
+            className="py-2 px-6 md:px-8 text-sm md:text-base text-white font-bold bg-blue-600 hover:bg-opacity-90 transition duration-300 rounded-full"
+          >
             Click Here
           </button>
         </Link>
-		</div>
-        
       </div>
     </div>
   </form>
