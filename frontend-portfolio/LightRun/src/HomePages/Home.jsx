c983f9101909b8d53fa4a60022f771697fd26172
TimiMaliki <malikitimi@gmail.com>
2025-07-05 10:58:21 +0100

    remove : navbar in homepage

--- frontend-portfolio/LightRun/src/HomePages/Home.jsx
+++ frontend-portfolio/LightRun/src/HomePages/Home.jsx
@@ -2,7 +2,6 @@ import About from "./about";
 import Clients from "./client";
 import Contact from "./contact";
 import Hero from "./heroSection";
-import Navbar from "../component/navbar";
 import PricingSection from "./pricing";
 import Services from "./services";
 import Studio from "./studio";
@@ -10,7 +9,6 @@ import Studio from "./studio";
 const Home = () => {
   return (
     <>
-      <Navbar />
       <Hero />
       <Studio />
       <Services />
