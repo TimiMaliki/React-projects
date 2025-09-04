fd0b48588d8587028f0843471875ec504c84db31
TimiMaliki <malikitimi@gmail.com>
2025-09-04 18:33:21 +0100

    added : search jobs pagination mobile display

--- frontend-portfolio/jobbers/src/Component/Hero.jsx
+++ frontend-portfolio/jobbers/src/Component/Hero.jsx
@@ -6,29 +6,29 @@ const Hero = () => {
       <div className="container-xl lg:container m-auto px-6 text-center mt-10 md:mt-24">
         {/* Heading */}
         <h1 className="text-5xl md:text-6xl font-extrabold text-gray-900 mb-2 md:mb-6">
-          Find Your <span className="bg-gradient-to-r from-green-600 to-yellow-500 bg-clip-text text-transparent">Perfect Job</span> Today
+          Find Your{" "}
+          <span className="bg-gradient-to-r from-green-600 to-yellow-500 bg-clip-text text-transparent">
+            Perfect Job
+          </span>
+          Today
         </h1>
 
         {/* Subheading */}
         <p className="text-lg md:text-xl text-gray-600 max-w-2xl mx-auto mb-10 leading-relaxed">
-          Discover opportunities that match your skills and schedule.  
-          Your next career move is just a click away.
+          Discover opportunities that match your skills and schedule. Your next
+          career move is just a click away.
         </p>
         <div className="flex flex-row sm:flex-row items-center justify-center gap-4">
           <Link to={"/search"}>
-          <div
-            className="bg-green-600 hover:bg-green-700 text-white px-3 md:px-8 py-2 md:py-3 rounded-xl font-medium shadow-md hover:shadow-lg transition-all duration-300"
-          >
-            Explore Remote Jobs
-          </div>
+            <div className="bg-green-600 hover:bg-green-700 text-white px-3 md:px-8 py-2 md:py-3 rounded-xl font-medium shadow-md hover:shadow-lg transition-all duration-300">
+              Explore Remote Jobs
+            </div>
           </Link>
-          
-          <Link to={"/add-job"} >
-          <div
-            className="bg-yellow-500 hover:bg-yellow-600 text-gray-900  px-3 md:px-8 py-2 md:py-3 rounded-xl font-medium shadow-md hover:shadow-lg transition-all duration-300"
-          >
-            Post a Job
-          </div>
+
+          <Link to={"/add-job"}>
+            <div className="bg-yellow-500 hover:bg-yellow-600 text-gray-900  px-3 md:px-8 py-2 md:py-3 rounded-xl font-medium shadow-md hover:shadow-lg transition-all duration-300">
+              Post a Job
+            </div>
           </Link>
         </div>
       </div>
