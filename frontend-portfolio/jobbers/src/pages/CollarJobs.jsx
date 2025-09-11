b96febca9f94d5550b599d5428d702deb54c556b
TimiMaliki <malikitimi@gmail.com>
2025-09-11 14:05:02 +0100

    changed collar job lisitng

--- frontend-portfolio/jobbers/src/pages/CollarJobs.jsx
+++ frontend-portfolio/jobbers/src/pages/CollarJobs.jsx
@@ -4,8 +4,13 @@ import Spinners from "../Component/Spinners";
 
 const CollarJobs = () => {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [mobileData, setMobileData] = useState([]);
+  const [loading, setLoading] = useState();
+  const [mobileloading, setMobileLoading] = useState(false);
 
+  {
+    /* Desktop Jobs */
+  }
   useEffect(() => {
     fetch("http://localhost:5000/jobs")
       .then((res) => res.json())
@@ -13,6 +18,20 @@ const CollarJobs = () => {
         console.log(data);
         setData(data);
         setLoading(false);
+      })
+      .catch((err) => console.error(err), setLoading(false));
+  }, []);
+
+  {
+    /* mobile Jobs */
+  }
+  useEffect(() => {
+    fetch("http://localhost:5000/jobs")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+        setMobileLoading(true);
       })
       .catch((err) => console.error(err), setLoading(false));
   }, []);
@@ -27,18 +46,46 @@ const CollarJobs = () => {
           {<Spinners loading={loading}/> ? (<Spinners loading={loading}/>) : ("") }
         </h2> */}
         {/* Jobs */}
+        <div className="md:block hidden">
+          {data && data.length > 0 ? (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {data.map((job) => (
+                <CollarJobListing job={job} key={job.id} />
+              ))}
+            </div>
+          ) : (
+            <div className="flex justify-center text-center items-center">
+              <Spinners loading={loading} />
+            </div>
+          )}
+        </div>
 
-        {data && data.length > 0 ? (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {data.map((job) => (
-              <CollarJobListing job={job} key={job.id} />
-            ))}
-          </div>
-        ) : (
-          <div className="flex justify-center text-center items-center">
-           <Spinners loading={loading}/>
-          </div>
-        )}
+        <div className="md:hidden block">
+          {mobileData && mobileData.length > 0 ? (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {data.map((job) => (
+                <CollarJobListing job={job} key={job.id} />
+              ))}
+            </div>
+          ) : (
+            <div className="text-center items-center grid">
+              <h2 className="text-center text-xl text-black">
+                <h2 className="text-lg font-bolder mb-4">Failed To fetch</h2>
+                <h2>
+                  Error:
+                  <br />
+                  📄 JSON / Mock API – To simulate job data and backend
+                  integration
+                </h2>
+                {<Spinners mobileloading={mobileloading} /> ? (
+                  <Spinners mobileloading={mobileloading} />
+                ) : (
+                  ""
+                )}
+              </h2>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
