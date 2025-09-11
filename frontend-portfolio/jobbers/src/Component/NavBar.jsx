f38a02f2a2c2e3dc341390f24f82c49bd80a3583
TimiMaliki <malikitimi@gmail.com>
2025-09-11 23:33:38 +0100

    changed the favicon

--- frontend-portfolio/jobbers/src/Component/NavBar.jsx
+++ frontend-portfolio/jobbers/src/Component/NavBar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
