73cbcc293b41d14aecc59d3eb0d708d18246d30e
TimiMaliki <malikitimi@gmail.com>
2025-09-11 13:13:09 +0100

    changed collar job lisitng

--- frontend-portfolio/jobbers/src/pages/CollarJobListing.jsx
+++ frontend-portfolio/jobbers/src/pages/CollarJobListing.jsx
@@ -5,7 +5,6 @@ import Spinners from "../Component/Spinners";
 
 const CollarJobListing = ({ job }) => {
   const [showMore, setShowMore] = useState(false);
-  const [loading, setLoading] = useState(true);
   const collarColors = {
     white: "text-gray-700 bg-gray-100",
     blue: "text-blue-700 bg-blue-100",
@@ -17,7 +16,6 @@ const CollarJobListing = ({ job }) => {
   return (
     <div className="bg-white rounded-2xl shadow-lg hover:shadow-2xl transition-shadow duration-300 overflow-hidden border border-green-400">
       {/* Header */}
-      {loading ? (
         <div className="p-6">
           <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between mb-4">
             <div>
@@ -78,13 +76,7 @@ const CollarJobListing = ({ job }) => {
             </div>
           </div>
         </div>
-      ) : (
-        <h1 className="p-6">
-          <Spinners loading={loading} />
-          <p className="text-center text-lg mb-5">Failed to fetch</p>
-          📄 JSON / Mock API – To simulate job data and backend integration
-        </h1>
-      )}
+      
     </div>
   );
 };
