19b5e16b5b0902a77b475abe0d2d8d8629dd30c3
TimiMaliki <malikitimi@gmail.com>
2025-09-18 06:05:44 +0100

    added: navbar

--- frontend-portfolio/ibook/src/App.js
+++ frontend-portfolio/ibook/src/App.js
@@ -1,15 +1,16 @@
 import gsap from "gsap";
 import { ScrollTrigger , SplitText } from "gsap/all";
 import "./App.css";
+import Navbar from "./component/Navbar";
 
 gsap.registerPlugin(ScrollTrigger , SplitText)
 
 function App() {
  
   return (
-    <div className="App h-[3000px]">
- hello word
-    </div>
+    <>
+ <Navbar/>
+    </>
   );
 }
 
