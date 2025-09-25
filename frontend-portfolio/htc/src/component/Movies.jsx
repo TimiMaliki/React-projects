6e296a1ef6474ed7eadacf41fa902298c6e3518d
TimiMaliki <malikitimi@gmail.com>
2025-09-25 15:02:35 +0100

    theatre

--- frontend-portfolio/htc/src/component/Movies.jsx
+++ frontend-portfolio/htc/src/component/Movies.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import gsap from 'gsap';
+import { useMediaQuery } from 'react-responsive'
+import { useGSAP } from '@gsap/react'
 import { featureLists, goodLists } from '../constants/index'
 const Movies = () => {
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+ 
+    useGSAP(() => {
+       const start = isMobile ? 'top 20%' : 'top top';
+       
+       const maskTimeline = gsap.timeline({
+        scrollTrigger: {
+           trigger: '#art',
+           start,
+           end: 'bottom center',
+           scrub: 1.5,
+           pin: true
+        }
+       })
+       
+       maskTimeline
+        .to('.will-fade', { opacity: 0, stagger: 0.2, ease: 'power1.inOut', })
+        .to('.masked-img', { scale: 1.3, maskPosition: 'center', maskSize: '700%', duration: 1, ease: 'power1.inOut '})
+        .to('#masked-content', { opacity: 1, duration: 1, ease: 'power1.inOut'})
+    })
+    
   return (
     <div id="art">
     <div className="container mx-auto h-full pt-20">
-       <h2 className="will-fade">Theatre</h2>
+       <h2 className="will-fade">Movies</h2>
        
        <div className="content">
         <ul className="space-y-4 will-fade">
@@ -18,9 +42,9 @@ const Movies = () => {
         
         <div className="cocktail-img">
            <img
-               src="../src/images/under-img.jpg"
+               src="../src/images/superhero-7524471.png"
                alt="cocktail"
-               className="abs-center masked-img size-full object-contain"
+               className="abs-center masked-img size-full object-contain overflow-hidden"
            />
         </div>
         
