9376c0e47dd581a1ee14226ee623f151da0f2902
TimiMaliki <malikitimi@gmail.com>
2025-10-26 16:00:26 +0100

    context-hooks

--- React Projects/blogPost-redux/src/App.jsx
+++ React Projects/blogPost-redux/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import AddPostForm from './features/AddForm'
 import PostsList  from './features/PostsList'
 function App() {
  
   return (
     <>
+    <AddPostForm/>
    <PostsList/>
     </>
   )
