8887d988ff73e1860b8c6aafdc8632f2a56c9e24
TimiMaliki <malikitimi@gmail.com>
2025-10-28 15:59:36 +0100

    added : uuid

--- React Projects/React-Context-and-Hooks/src/component/NotesList.jsx
+++ React Projects/React-Context-and-Hooks/src/component/NotesList.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React ,  { useState }  from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 const NotesList = () => {
+  const [book, setBooks] = useState([
+    { title: 'almost home', id: 1 },
+    { title: 'memory gospel', id: 2 },
+    { title: 'this wild darkness', id: 3 }
+  ]);
+  const addSong = () => {
+  setBooks([...book, {title: 'new book', id:uuidv4()}])
+  }
+
   return (
     <div className="book-list">
     <ul>
-      <li>the way of kings</li>
-      <li>the name of the wind</li>
-      <li>the final empire</li>
+     {book.map(book => (
+      <li key ={book.id}>{book.title}</li>
+     ))}
     </ul>
+    <button onClick={addSong}>Add  a book</button>
   </div>
   )
 }
