59e73de4da03a3e248c03b336ae4c8cd30e6ad87
TimiMaliki <malikitimi@gmail.com>
2025-10-30 14:49:14 +0100

    ...

--- React Projects/React-Context-and-Hooks/src/component/NoteForm.jsx
+++ React Projects/React-Context-and-Hooks/src/component/NoteForm.jsx
@@ -19,3 +19,5 @@ const NoteForm = ({addSong}) => {
 }
 
 export default NoteForm
+
+
--- React Projects/React-Context-and-Hooks/src/component/ThemeToggle.jsx
+++ React Projects/React-Context-and-Hooks/src/component/ThemeToggle.jsx
@@ -10,15 +10,3 @@ export default ThemeToggle;
 
 
 
-// import React, { Component } from 'react';
-// import { ThemeContext } from '../contexts/ThemeContext';
-
-// class ThemeToggle extends Component {
-//   static contextType = ThemeContext;
-//   render() { 
-//     const { toggleTheme } = this.context;
-//     return ( <button onClick={toggleTheme}>Toggle the theme</button>);
-//   }
-// }
- 
-// export default ThemeToggle;
