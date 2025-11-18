fe8e06fa88285349a153345b4e91b1eaf8fb21a6
TimiMaliki <malikitimi@gmail.com>
2025-11-18 19:29:02 +0100

    added: contact us email

--- React Projects/meds/src/Pages/EmailForm.jsx
+++ React Projects/meds/src/Pages/EmailForm.jsx
@@ -9,7 +9,54 @@ function EmailForm() {
     <form className="grid grid-cols-1 md:grid-cols-2 gap-6" style={{ background: themeStyle.bg, color: themeStyle.hex }}>
       <h1 className="text-2xl font-bold md:col-span-2">CONTACT US</h1>
 
-      <input
+
+
+
+        <div className="grid grid-cols-2 gap-6 md:col-span-2">
+        
+          <a href="mailto:support@muhameds.com" className="block  mb-6 transition">
+            support@muhameds.com
+          </a>
+
+        
+          <div className="flex space-x-4">
+            <a
+              href="https://www.instagram.com/muhameds/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://cdn.prod.website-files.com/62282607739bd61f2cabc5ee/625718812262cd66c86bbee6_Gold-Metallic-Logo-IG.png"
+                alt="Instagram"
+                className="w-10 h-10 hover:scale-110 transition-transform"
+              />
+            </a>
+            <a
+              href="https://x.com/muhamedsglobal"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://cdn.prod.website-files.com/62282607739bd61f2cabc5ee/674f4ccc2984b77982208a00_X-GoldMetalic-icon-600.png"
+                alt="Twitter/X"
+                className="w-10 h-10 hover:scale-110 transition-transform"
+              />
+            </a>
+            <a
+              href="https://weedmaps.com/brands/muha-meds/products?isNearby=true"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://cdn.prod.website-files.com/62282607739bd61f2cabc5ee/625c6abfc8a26a8857c3e963_Gold-WM.png"
+                alt="Weedmaps"
+                className="w-10 h-10 hover:scale-110 transition-transform"
+              />
+            </a>
+          </div>
+        </div>
+
+      {/* <input
         type="text"
         placeholder="Name *"
         className="border p-3 rounded-lg w-full text-lg"
@@ -27,20 +74,20 @@ function EmailForm() {
         type="text"
         placeholder="Phone Number"
         className="border p-3 rounded-lg w-full text-lg"
-      />
+      /> */}
 
       {/* Inquiry Type */}
-      <div className="md:col-span-2">
+      {/* <div className="md:col-span-2">
         <label className="block mb-2 text-lg font-semibold">Type of Inquiry *</label>
         <select className="border p-3 rounded-lg w-full">
           <option>Dispensary / Delivery</option>
           <option>Customer</option>
           <option>Wholesale</option>
         </select>
-      </div>
+      </div> */}
 
       {/* State */}
-      <div className="md:col-span-2">
+      {/* <div className="md:col-span-2">
         <label className="block mb-2 text-lg font-semibold">State *</label>
         <select className="border p-3 rounded-lg w-full text-lg">
           {states.map((state) => (
@@ -63,7 +110,7 @@ function EmailForm() {
 
       <button class="bg-black text-white px-7 py-3 rounded-lg mt-4 md:col-span-2 text-lg">
         Submit
-      </button>
+      </button> */}
     </form>
   );
 }
